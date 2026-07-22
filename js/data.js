b33d779f1b43d199a526f190378fc68ee48c2025
Danica Jovanovic <danicadsgn@gmail.com>
2026-07-22 22:56:32 +0200

    Initial KROSH website

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const bags = [
+  { id: "pink", color: "#F44A8E", label: "Hot Pink" },
+  { id: "brightgreen", color: "#055834", label: "Emerald" },
+  { id: "white", color: "#F6F6F6", label: "White" },
+  { id: "brown", color: "#592223", label: "Cocoa" },
+  { id: "lightolive", color: "#58816E", label: "Olive" },
+  { id: "babypink", color: "#EFAFC6", label: "Baby Pink" },
+  { id: "darkblue", color: "#0043C0", label: "Royal Blue" },
+  { id: "yellow", color: "#E4DAA7", label: "Butter" },
+  { id: "darkgreen", color: "#0C463A", label: "Forest" },
+  { id: "red", color: "#D71331", label: "Red" },
+  { id: "black", color: "#0F0F0F", label: "Black" },
+  { id: "beige", color: "#E9E1CB", label: "Beige" },
+  { id: "darkolive", color: "#3D462C", label: "Moss" }
+];
+const pouches = [
+  { id: "bluefloral", image: "images/pouches/bluefloral.webp" },
+  { id: "blueplaid", image: "images/pouches/blueplaid.webp" },
+  { id: "fruit", image: "images/pouches/fruit.webp" },
+  { id: "pinkplaid", image: "images/pouches/pinkplaid.webp" },
+  { id: "pinkstripes", image: "images/pouches/pinkstripes.webp" },
+  { id: "purple", image: "images/pouches/purple.webp" },
+  { id: "redcheck", image: "images/pouches/redcheck.webp" },
+  { id: "redplaid", image: "images/pouches/redplaid.webp" },
+  { id: "softpink", image: "images/pouches/softpink.webp" },
+  { id: "thinpink", image: "images/pouches/thinpink.webp" },
+  { id: "yellow", image: "images/pouches/yellow.webp" },
+  { id: "garden", image: "images/pouches/garden.webp" },
+  { id: "corduroy", image: "images/pouches/corduroy.webp" }
+];
---
+++ js/script.js
@@ -0,0 +1,140 @@
+const bagOptionsContainer = document.querySelector("#bag-options");
+const selectedBagLabel = document.querySelector("#selected-bag-label");
+const pouchOptionsContainer = document.querySelector("#pouch-options");
+const combinationImage = document.querySelector("#combination-image");
+
+let selectedBag = "pink";
+let selectedPouch = "fruit";
+
+function updateCombinationImage() {
+  const newImagePath =
+    `images/combinations/bag-${selectedBag}_pouch-${selectedPouch}.webp`;
+
+  combinationImage.classList.add("is-changing");
+
+  setTimeout(() => {
+    combinationImage.src = newImagePath;
+  }, 100);
+
+  combinationImage.onload = () => {
+    combinationImage.classList.remove("is-changing");
+  };
+}
+
+function renderBagOptions() {
+  bagOptionsContainer.innerHTML = "";
+
+  bags.forEach((bag) => {
+    const button = document.createElement("button");
+
+    button.type = "button";
+    button.classList.add("color-option");
+    button.style.backgroundColor = bag.color;
+    button.setAttribute("aria-label", bag.label);
+
+    if (bag.id === selectedBag) {
+      button.classList.add("selected");
+    }
+
+    button.addEventListener("click", () => {
+  selectedBag = bag.id;
+  selectedBagLabel.textContent = bag.label;
+
+  renderBagOptions();
+
+  const selectedButton =
+    bagOptionsContainer.querySelector(".color-option.selected");
+
+  selectedButton.classList.add("pop");
+
+  updateCombinationImage();
+});
+
+    bagOptionsContainer.appendChild(button);
+  });
+}
+
+function renderPouchOptions() {
+  pouchOptionsContainer.innerHTML = "";
+
+  pouches.forEach((pouch) => {
+    const button = document.createElement("button");
+
+    button.type = "button";
+    button.classList.add("pouch-option");
+
+    if (pouch.id === selectedPouch) {
+      button.classList.add("selected");
+    }
+
+    const image = document.createElement("img");
+    image.src = pouch.image;
+    image.alt = "";
+    image.draggable = false;
+
+    button.appendChild(image);
+
+    button.addEventListener("click", () => {
+  selectedPouch = pouch.id;
+
+  renderPouchOptions();
+
+  const selectedButton =
+    pouchOptionsContainer.querySelector(".pouch-option.selected");
+
+  selectedButton.classList.add("pop");
+
+  updateCombinationImage();
+});
+    pouchOptionsContainer.appendChild(button);
+  });
+}
+
+selectedBagLabel.textContent =
+  bags.find((bag) => bag.id === selectedBag).label;
+
+renderBagOptions();
+renderPouchOptions();
+updateCombinationImage();
+
+function setupFade(wrapperSelector, rowSelector) {
+  const wrapper = document.querySelector(wrapperSelector);
+  const row = document.querySelector(rowSelector);
+
+  function updateFade() {
+    const atEnd =
+      row.scrollLeft + row.clientWidth >= row.scrollWidth - 2;
+
+    wrapper.classList.toggle("at-end", atEnd);
+  }
+
+  row.addEventListener("scroll", updateFade);
+
+  updateFade();
+}
+setupFade("#bag-wrapper", "#bag-options");
+setupFade("#pouch-wrapper", "#pouch-options");
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+    const isOpen = item.classList.contains("active");
+
+    faqItems.forEach((otherItem) => {
+      otherItem.classList.remove("active");
+
+      const otherQuestion = otherItem.querySelector(".faq-question");
+      otherQuestion.setAttribute("aria-expanded", "false");
+    });
+
+    if (!isOpen) {
+      item.classList.add("active");
+      question.setAttribute("aria-expanded", "true");
+    }
+  });
+
+  question.setAttribute("aria-expanded", "false");
+});
