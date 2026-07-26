566eb56400e6f5eb28247e38d42f19a260169009
Danica Jovanovic <danicadsgn@gmail.com>
2026-07-26 15:37:18 +0200

    Update sold out items

--- js/data.js
+++ js/data.js
@@ -11,7 +11,7 @@ const bags = [
   { id: "red", color: "#D71331", label: "Red", soldOut: false },
   { id: "black", color: "#0F0F0F", label: "Black", soldOut: false },
   { id: "beige", color: "#E9E1CB", label: "Beige", soldOut: false },
-  { id: "darkolive", color: "#3D462C", label: "Moss", soldOut: false }
+  { id: "darkolive", color: "#3D462C", label: "Moss", soldOut: true }
 ];
 
 const pouches = [
@@ -26,6 +26,6 @@ const pouches = [
   { id: "softpink", image: "images/pouches/softpink.webp", soldOut: false },
   { id: "thinpink", image: "images/pouches/thinpink.webp", soldOut: false },
   { id: "yellow", image: "images/pouches/yellow.webp", soldOut: false },
-  { id: "garden", image: "images/pouches/garden.webp", soldOut: false },
+  { id: "garden", image: "images/pouches/garden.webp", soldOut: true },
   { id: "corduroy", image: "images/pouches/corduroy.webp", soldOut: false }
 ];
